8854c6bf46f4ae5ea511805747d15b54cad91c91
admir.huseini <admir@gauss-school.com>
2019-11-02 20:29:45 +0100

    Fix prodess material reference issue

--- src/app/shared/imagecard/imagecard.component.ts
+++ src/app/shared/imagecard/imagecard.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ImagecardComponent implements OnInit {
   @Input() title: string;
-  @Input() desc: string;
+  //@Input() desc: string;
   @Input() footerTitle: string;
   @Input() position: string;
   @Input() image: string;
